58c3eeb45506d8c8d91d90a74a13a129e2adee0c
rahmi87 <81910399+rahmi87@users.noreply.github.com>
2022-11-09 19:44:03 +0700

    Add files via upload

---
+++ project berpasangan/DaftarHarga.java
@@ -0,0 +1,20 @@
+interface daftar{
+    public void daftar();
+}
+
+public class DaftarHarga implements daftar
+{
+    public void daftar() {
+        System.out.println("=================Jenis kamar dan harga=====================");
+        System.out.println("1. AC + kamar mandi dalam\t Rp. 500.000- \t perbulan");
+        System.out.println("2. AC + kamar mandi luar\t Rp. 400.000- \t perbulan");
+        System.out.println("3. kipas + kamar mandi dalam\t Rp. 300.000- \t perbulan");
+        System.out.println("4. kipas + kamar mandi luar\t Rp. 250.000- \t perbulan");
+
+
+    }
+    
+}
+     
+
+
---
+++ project berpasangan/awaldata.java
@@ -0,0 +1,5 @@
+public abstract class awaldata {
+     protected abstract void data();
+     protected abstract void harga();
+     protected abstract void output();
+}
